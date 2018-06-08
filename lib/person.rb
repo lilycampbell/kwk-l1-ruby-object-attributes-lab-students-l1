eea12c30b88493852e910c792feaaffe81d8fab0
lilycampbell <lilycatcampbell@gmail.com>
2018-06-08 19:59:19 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,2 +1,30 @@
 # person.rb
-class person
+class Person
+  def initialize(name, job)
+    @name = name
+    @job = job
+  end 
+  
+  def name=(name)
+  @name = name
+end 
+
+def job=(job)
+  @job = job
+end
+    
+    def name
+      @name 
+    end
+    
+    def job
+      @job
+    end
+    
+end
+   
+person_one = Person.new("fido", "dog")
+
+# dog_one.name = "Buddy"
+
+puts person_one.name 
